ccf2de8722927afdb19b21a29cc2fd5e616312b4
signeklinting <klinting.signe@gmail.com>
2021-01-15 20:19:45 +0100

    Dec6

--- aoc2017/06dec/06.py
+++ aoc2017/06dec/06.py
@@ -1,4 +1,29 @@
 from collections import defaultdict
 
-for line in open('06.sample', 'r+').readlines():
-	line = line.strip()
+#banks = [int(i) for i in open('06.sample', 'r+').readline().strip().split(' ')]
+banks = [int(i) for i in open('06.in', 'r+').readline().strip().split('\t')]
+
+
+current_state = list(banks)
+reallocations = 0
+repeated = 0
+states = [list(current_state)]
+
+# Puzzle 1 & 2
+while repeated < 2:
+	i_highest, blocks = current_state.index(max(current_state)), max(current_state)
+	reallocations += 1
+	
+	current_state[i_highest] = 0
+
+	for i in range(1, blocks + 1):
+		current_state[(i_highest + i) % len(current_state)] += 1
+	
+	if current_state in states:
+		print(reallocations)
+		repeated += 1
+		reallocations = 0
+		states = []
+	
+	states.append(list(current_state))
+
