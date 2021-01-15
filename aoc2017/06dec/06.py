113552f5bf46d9a755cef483601f54b2128c7886
signeklinting <klinting.signe@gmail.com>
2021-01-15 20:31:52 +0100

    Dec6 corrected

--- aoc2017/06dec/06.py
+++ aoc2017/06dec/06.py
@@ -3,27 +3,29 @@ from collections import defaultdict
 #banks = [int(i) for i in open('06.sample', 'r+').readline().strip().split(' ')]
 banks = [int(i) for i in open('06.in', 'r+').readline().strip().split('\t')]
 
-
-current_state = list(banks)
 reallocations = 0
-repeated = 0
-states = [list(current_state)]
+states = set()
 
-# Puzzle 1 & 2
-while repeated < 2:
-	i_highest, blocks = current_state.index(max(current_state)), max(current_state)
-	reallocations += 1
+while tuple(banks) not in states:
+	states.add(tuple(banks))
+	i_highest, blocks = banks.index(max(banks)), max(banks)
+	banks[i_highest] = 0
 	
-	current_state[i_highest] = 0
-
 	for i in range(1, blocks + 1):
-		current_state[(i_highest + i) % len(current_state)] += 1
-	
-	if current_state in states:
-		print(reallocations)
-		repeated += 1
-		reallocations = 0
-		states = []
-	
-	states.append(list(current_state))
+		banks[(i_highest + i) % len(banks)] += 1
+	reallocations += 1
 
+print(reallocations)
+state = tuple(banks)
+new_reallocations = 0
+while True:
+	i_highest, blocks = banks.index(max(banks)), max(banks)
+	banks[i_highest] = 0
+	
+	for i in range(1, blocks + 1):
+		banks[(i_highest + i) % len(banks)] += 1
+	new_reallocations += 1
+
+	if tuple(banks) == state:
+		print(new_reallocations)
+		break
